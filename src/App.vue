9a7c788f1ddc45900be4f973cbe63b30276d6ae9
Osman Pérez <operez@edutecno.com>
2022-05-30 12:07:04 -0400

    Commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+      <v-data-table
+        :headers="headers"
+        :items="data"
+        :options.sync="options"
+        :server-items-length="totalDesserts"
+        :loading="loading"
+        class="elevation-1"
+      ></v-data-table>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "ListApside",
+  props: {
+    url: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Id',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        {
+          text: 'Body',
+          align: 'start',
+          sortable: false,
+          value: 'body',
+        },
+        {
+          text: 'Title',
+          align: 'start',
+          sortable: false,
+          value: 'title',
+        }
+      ],
+      data: [],
+      totalDesserts: 0,
+      loading: true,
+      options: {},
+    }
+  },
+  watch: {
+    options: {
+      handler () {
+        this.getRecords()
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    getRecords () {
+      this.loading = true
+      const start = this.options.page * this.options.itemsPerPage - this.options.itemsPerPage
+      axios.get(this.url,{
+        params:{
+          _start: start,
+          _limit: this.options.itemsPerPage
+        }
+      }).then(async ({data}) => {
+        const totalPage = (await axios.get(this.url)).data.length
+        this.data = data
+        this.totalDesserts = totalPage
+        this.loading = false
+      })
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from '@/plugins/vuetify' // path to vuetify expor
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App, {props:{ 'url': 'https://jsonplaceholder.typicode.com/posts'}}),
 }).$mount('#app')
